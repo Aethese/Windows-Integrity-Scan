b2daac2bc5c363dc4a328465926a83bffb22d960
Aethese <59108437+Aethese@users.noreply.github.com>
2022-08-09 11:28:15 -0500

    Add files via upload

---
+++ scan.bat
@@ -0,0 +1,25 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+	echo Administrative permissions required. Detecting permissions...
+	
+	net session >nul 2>&1
+	if %errorLevel% == 0 (
+		echo Success: Administrative permissions confirmed. Running scan.
+		sfc /scannow
+		echo Scan one complete, now scanning health.
+
+		dism /Online /Cleanup-image /Scanhealth
+		echo Scan two complete, now checking health.
+
+		dism /Online /Cleanup-Image /CheckHealth
+		echo Scan three complete, now restoring health.
+
+		dism /Online /Cleanup-image /Restorehealth
+		echo Scans completed.
+	) else (
+		echo Failure: Current permissions inadequate. Please run as Administrator.
+	)
+	
+	pause >nul
